3c1376edbd6a732d16952ea8c6b642bf6bfa6779
dennis23314063 <f74092081@gs.ncku.edu.tw>
2021-12-25 17:56:13 +0800

    test

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models.send_messages import ImageSendMessage
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -16,7 +17,11 @@ def send_text_message(reply_token, text):
 
 def send_image_url(id, img_url):
     line_bot_api =LineBotApi(channel_access_token)
-    line_bot_api.reply_message(id, send_image_url(img_url=img_url))
+    image_message = ImageSendMessage(
+        original_content_url='https://github.com/dennis23314063/test/blob/master/fsm.png?raw=true',
+        preview_image_url='https://github.com/dennis23314063/test/blob/master/fsm.png?raw=true'
+    )
+    line_bot_api.reply_message(id, image_message)
     pass
 
 def send_button_message(id, text, buttons):
