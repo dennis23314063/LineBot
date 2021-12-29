6319bfc222908b506cd9bf8d95cafa256793366e
dennis23314063 <f74092081@gs.ncku.edu.tw>
2021-12-29 15:46:31 +0800

    test

--- app.py
+++ app.py
@@ -56,32 +56,32 @@ line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
 
-# @app.route("/callback", methods=["POST"])
-# def callback():
-#     signature = request.headers["X-Line-Signature"]
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-#     # parse webhook body
-#     try:
-#         events = parser.parse(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-#     # if event is MessageEvent and message is TextMessage, then echo text
-#     for event in events:
-#         if not isinstance(event, MessageEvent):
-#             continue
-#         if not isinstance(event.message, TextMessage):
-#             continue
-#         if event.message.text.lower() == 'fsm':
-#             send_image_url(event.reply_token, 'https://github.com/dennis23314063/test/blob/master/fsm.png?raw=true')
-#         line_bot_api.reply_message(
-#             event.reply_token, TextSendMessage(text=event.message.text)
-#         )
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+        if event.message.text.lower() == 'fsm':
+            send_image_url(event.reply_token, 'https://github.com/dennis23314063/test/blob/master/fsm.png?raw=true')
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text)
+        )
 
-#     return "OK"
+    return "OK"
 
 
 @app.route("/webhook", methods=["POST"])
