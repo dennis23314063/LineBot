d04aeb4b655b83263f1d0f52ad9cc744e6d9f409
dennis23314063 <f74092081@gs.ncku.edu.tw>
2021-12-30 06:30:54 +0800

    fsm

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def show_fsm():
     return send_file('fsm.png',mimetype='image/png')
 
 if __name__ == "__main__":
-    show_fsm()
+    # show_fsm()
     port = os.environ.get("PORT", 8000)
     app.run(host="0.0.0.0", port=port, debug=True)
     
