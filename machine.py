76c466a2e264d4f139dae5c80260d8918a400054
dennis23314063 <f74092081@gs.ncku.edu.tw>
2021-12-30 02:32:28 +0800

    state update

---
+++ machine.py
@@ -0,0 +1,51 @@
+from fsm import TocMachine
+def multiple_machine():
+    machine = TocMachine(
+    states=["user", "menu", "introduction","draw","fsm"],
+    transitions=[
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "menu",
+            "conditions": "is_going_to_menu",
+        },
+        {
+            "trigger": "advance",
+            "source": "menu",
+            "dest": "introduction",
+            "conditions": "is_going_to_introduction",
+        },
+        {
+            "trigger": "go_back",
+            "source":["introduction","fsm"],
+            "dest": "menu"
+        },
+        {
+            "trigger": "go_back",
+            "source":"draw",
+            "dest": "draw"
+        },
+        {
+            "trigger": "advance",
+            "source":"menu",
+            "dest": "fsm",
+            "conditions": "is_going_to_fsm",
+        },
+        {
+            "trigger": "advance",
+            "source":"menu",
+            "dest": "draw",
+            "conditions": "is_going_to_draw",
+        },
+        {
+            "trigger": "advance",
+            "source":"draw",
+            "dest": "menu",
+            "conditions": "is_going_to_menu",
+        },
+    ],
+    initial="user",
+    auto_transitions=False,
+    show_conditions=True,
+    )
+    return machine
